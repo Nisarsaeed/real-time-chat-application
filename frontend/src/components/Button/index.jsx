141a01c1e765dd485eb56ba0507c48c34f4dac7f
Nisar Saeed <dash75549@gmail.com>
2024-07-27 00:46:27 +0500

    update: icon prop added

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -4,7 +4,8 @@ export const Button = ({
   type = 'button',
   className = '',
   title = 'Submit',
-  onClick = () => {}
+  onClick = () => {},
+  icon,
 }) => {
   return (
     <button
@@ -12,7 +13,8 @@ export const Button = ({
       className={`bg-primary hover:bg-primary-light focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 my-3 focus:outline-none text-white w-1/2 ${className}`}
       onClick={onClick}
     >
-      {title}
+    {title}
+    {icon} 
     </button>
   );
 };
@@ -21,5 +23,7 @@ Button.propTypes = {
   type: PropTypes.string.isRequired,
   className: PropTypes.string,
   title: PropTypes.string.isRequired,
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  icon: PropTypes.any
+
 };
