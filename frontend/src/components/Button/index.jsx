c12b43a0b1a711de030c1fdf7d63ab2f3fba2e09
Nisar Saeed <dash75549@gmail.com>
2024-07-26 23:59:17 +0500

    update: props and types added

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -1,11 +1,25 @@
+import PropTypes from 'prop-types';
 
 export const Button = ({
-    type='',
-    className='',
+  type = 'button',
+  className = '',
+  title = 'Submit',
+  onClick = () => {}
+}) => {
+  return (
+    <button
+      type={type}
+      className={`bg-primary hover:bg-primary-light focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 my-3 focus:outline-none text-white w-1/2 ${className}`}
+      onClick={onClick}
+    >
+      {title}
+    </button>
+  );
+};
 
-})=>{
-
-    return(
-        <button type={type} className={` bg-primary hover:bg-primary-light focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 my-3 focus:outline-none text-white w-1/2  ${className}`}>Submit</button>
-    );
-}
+Button.propTypes = {
+  type: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func
+};
