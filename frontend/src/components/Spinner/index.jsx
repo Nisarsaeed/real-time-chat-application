7dc94bcbb30f2bc54cc68e44844b5eb8de02fe26
Nisar Saeed <dash75549@gmail.com>
2024-08-02 19:58:38 +0500

    fix: spinner color

--- frontend/src/components/Spinner/index.jsx
+++ frontend/src/components/Spinner/index.jsx
@@ -4,7 +4,7 @@ export const Spinner = ({display, isVariantSm}) => {
   return (
     <div className={` z-50 ${display} ${isVariantSm ?'h-7 w-7 inline':'w-full h-full fixed top-0 left-0 bg-white opacity-75'}`}>
       <div className={`  ${isVariantSm? 'inline': 'mt-[50vh] flex justify-center items-center'}`}>
-       <div className={`fas fa-circle-notch fa-spin text-primary ${isVariantSm?'text-black font-bold fa-xl':'fa-4x'}`}></div>
+       <div className={`fas fa-circle-notch fa-spin text-primary ${isVariantSm?' text-black font-bold fa-xl':'fa-4x'}`}></div>
       </div>
     </div>
   )
